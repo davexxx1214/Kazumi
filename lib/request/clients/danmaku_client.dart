1b44f18406614128139692d3d3d0cc0b0fd3a9d0
davexxx <davexxx@163.com>
2026-05-27 17:49:03 +0800

    fix(danmaku): require credentials for signed API builds

--- lib/request/clients/danmaku_client.dart
+++ lib/request/clients/danmaku_client.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:kazumi/request/core/dio_factory.dart';
 import 'package:kazumi/request/core/network_error_mapper.dart';
+import 'package:kazumi/services/logging/logger.dart';
 import 'package:kazumi/utils/dandan_credentials.dart';
 import 'package:kazumi/utils/http_headers.dart';
 import 'package:kazumi/utils/crypto.dart';
@@ -9,6 +10,10 @@ class DanmakuClient {
   DanmakuClient._();
 
   static final DanmakuClient instance = DanmakuClient._();
+  static bool _placeholderWarningLogged = false;
+
+  static const _placeholderAppId = 'kvpx7qkqjh';
+  static const _placeholderAppSecret = 'rABUaBLqdz7aCSi3fe88ZDj2gwga9Vax';
 
   Future<dynamic> get(
     String url, {
@@ -18,6 +23,7 @@ class DanmakuClient {
   }) async {
     final timestamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
     final uri = Uri.parse(url);
+    _logPlaceholderCredentialsIfNeeded();
     final requestHeaders = <String, dynamic>{
       'user-agent': getRandomUA(),
       'referer': '',
@@ -37,7 +43,28 @@ class DanmakuClient {
       );
       return response.data;
     } on DioException catch (e) {
+      final statusCode = e.response?.statusCode;
+      final danDanError = e.response?.headers.value('x-error-message');
+      KazumiLogger().w(
+        'DanmakuClient: request failed ${statusCode ?? e.type.name} ${uri.path}'
+        '${danDanError == null ? '' : ' ($danDanError)'}',
+        error: e.message,
+      );
       throw await NetworkErrorMapper.mapException(e);
+    }
+  }
+
+  void _logPlaceholderCredentialsIfNeeded() {
+    if (_placeholderWarningLogged) {
+      return;
+    }
+    if (dandanCredentials['id'] == _placeholderAppId &&
+        dandanCredentials['value'] == _placeholderAppSecret) {
+      _placeholderWarningLogged = true;
+      KazumiLogger().w(
+        'DanmakuClient: placeholder DanDan API credentials are in use; '
+        'release builds must inject DANDANAPI_APPID and DANDANAPI_KEY.',
+      );
     }
   }
 }
